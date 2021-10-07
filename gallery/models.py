994229ac37ace64e749ee850ce7f21156bef362c
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-10-07 19:39:52 +0300

    add relationships to models

--- gallery/models.py
+++ gallery/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Image(models.Model):
-    image_name=models.CharField(max_length=30)
-    image_description=models.TextField()
-    image=models.ImageField(upload_to='photo_gallery/', default=None)
-    
 
 class Location(models.Model):
-    location_name=models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
 
 class Category(models.Model):
-    category_name=models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photo_gallery/', default=None)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category=models.ManyToManyField(Category)  
--- photogallery/settings.py
+++ photogallery/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'photogallery.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+        'PASSWORD':'Mjamaica101',
     }
 }
 
