e017519f002ffcac38cc296a2dade7c8a8db7d8f
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-10-08 16:06:59 +0300

    add the view and templates for the home page

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Image
 
 # Create your views here.
+
+def all_images(request):
+    images=Image.objects.all()
+    return render(request, 'gallery/home.html', {'images':images})
